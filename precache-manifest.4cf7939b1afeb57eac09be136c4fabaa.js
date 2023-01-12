c919a9fc34d99832eb70434f2658e9095a95e95f
fecrakib <rakibislam@132575gmial.com>
2023-01-12 12:58:50 +0600

    Updates

---
+++ precache-manifest.4cf7939b1afeb57eac09be136c4fabaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ef9a6de9b76d734931",
+    "url": "/fecrakib.github.io/thesis/static/css/main.0f6d759a.chunk.css"
+  },
+  {
+    "revision": "35ef9a6de9b76d734931",
+    "url": "/fecrakib.github.io/thesis/static/js/main.2dd95768.chunk.js"
+  },
+  {
+    "revision": "e39d0792bbc62c23f43d",
+    "url": "/fecrakib.github.io/thesis/static/js/runtime~main.c96c1a44.js"
+  },
+  {
+    "revision": "712e24d55f396d8a2c2c",
+    "url": "/fecrakib.github.io/thesis/static/css/2.5640d4bf.chunk.css"
+  },
+  {
+    "revision": "712e24d55f396d8a2c2c",
+    "url": "/fecrakib.github.io/thesis/static/js/2.07464c03.chunk.js"
+  },
+  {
+    "revision": "dc846958103a70a61b94860bd2b704c3",
+    "url": "/fecrakib.github.io/thesis/static/media/b3.dc846958.jpg"
+  },
+  {
+    "revision": "8f5bf972709782bb687f4785b5589020",
+    "url": "/fecrakib.github.io/thesis/static/media/hiw.8f5bf972.jpg"
+  },
+  {
+    "revision": "df82856f76ae02bc7da9d97b5561a51f",
+    "url": "/fecrakib.github.io/thesis/static/media/cert.df82856f.png"
+  },
+  {
+    "revision": "4cddf33feccfc5101c3158892dbea5b6",
+    "url": "/fecrakib.github.io/thesis/index.html"
+  }
+];
